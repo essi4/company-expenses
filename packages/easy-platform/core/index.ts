cad2fb9d532fbf30620dcbbf01ea1a1e8ad9323d
essi4 <essi7688@gmail.com>
2026-09-20 22:16:32 +0330

    feat: expose EASY platform core contract

---
+++ packages/easy-platform/core/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./business-modules";
+export * from "./business-blueprints";
+export * from "./field-registry";
+export * from "./workflows";
+export * from "./permissions";
+export * from "./financial";
